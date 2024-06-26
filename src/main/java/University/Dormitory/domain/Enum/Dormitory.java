785627d62a3d296f287d9287c1e11d44ee124276
kwon-halim <blizzard777@gachon.ac.kr>
2024-06-26 23:22:46 +0900

    #2 기본 엔티티들 정의

---
+++ src/main/java/University/Dormitory/domain/Enum/Dormitory.java
@@ -0,0 +1,5 @@
+package University.Dormitory.domain.Enum;
+
+public enum Dormitory {
+    BUILDING1, BUILDING2, BUILDING3
+}
---
+++ src/main/java/University/Dormitory/domain/Enum/Role.java
@@ -0,0 +1,5 @@
+package University.Dormitory.domain.Enum;
+
+public enum Role {
+    ASSISTANT, SCHEDULE_ASSISTANT, PERFECT
+}
