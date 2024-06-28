562ca57ef6aeba8a1d5d0cf941094ff41d8675ef
kwon-halim <blizzard777@gachon.ac.kr>
2024-06-28 19:01:24 +0900

    #6 Service Serivce 필요할 것 같은 로직들

---
+++ src/main/java/University/Dormitory/service/UserAuthorityService/UserAuthoritycommandService.java
@@ -0,0 +1,13 @@
+package University.Dormitory.service.UserAuthorityService;
+
+public interface UserAuthoritycommandService {
+//    스케줄 권한 추가 기능(ROLE_ASSISTANT -> ROLE_SCHEDULE_ASSISTANT 변경 가능)
+    
+//    스케줄 권한 뺏기 기능(ROLE_SCHEDULE_ASSISTANT -> ROLE_ASSISTANT 변경 기능)
+
+//    사감 권한 추가 기능
+
+//    사감 권한 제거 기능
+
+
+}
