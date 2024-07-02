42757710606c4d82cbc04305d9161fff1131b288
kwon-halim <blizzard777@gachon.ac.kr>
2024-07-02 23:48:06 +0900

    #2 User: 클래스 파일 수정 UserAlarm: 알람이 필요한 기능이 있어서 추가 UserAuthorities: Security에 필요한 함수들 추가

---
+++ src/main/java/University/Dormitory/domain/UserAlarm.java
@@ -0,0 +1,21 @@
+package University.Dormitory.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class UserAlarm {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private String Message;
+}
