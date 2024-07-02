9db414044939dbe1c496281ed90cbf48b86f5daa
kwon-halim <blizzard777@gachon.ac.kr>
2024-07-02 23:49:41 +0900

    #5 필요없는 Import문 삭제

--- src/main/java/University/Dormitory/repository/JPARepository/UserAuthoritiesRepository.java
+++ src/main/java/University/Dormitory/repository/JPARepository/UserAuthoritiesRepository.java
@@ -1,13 +1,10 @@
 package University.Dormitory.repository.JPARepository;
 
-import University.Dormitory.domain.User;
 import University.Dormitory.domain.UserAuthorities;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.security.core.userdetails.UserDetails;
-
-import java.util.List;
 
 public interface UserAuthoritiesRepository extends JpaRepository<UserAuthorities, Long> {
     @Query("SELECT ua FROM UserAuthorities ua WHERE ua.user.userId = :userId")
