c1ba9decba2720f4cafd11f6d1291245cfcf5f84
kwon-halim <blizzard777@gachon.ac.kr>
2024-07-03 15:17:06 +0900

    #3 예외 파일 추가. 권한 못찾았을떄의 예외

---
+++ src/main/java/University/Dormitory/exception/UndefinedAuthorityException.java
@@ -0,0 +1,7 @@
+package University.Dormitory.exception;
+
+public class UndefinedAuthorityException extends RuntimeException {
+    public UndefinedAuthorityException(String message) {
+        super(message);
+    }
+}
