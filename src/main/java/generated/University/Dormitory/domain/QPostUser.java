dc3fa9b6a1985c818d0c44f8acf1eef1726cb74a
kwon-halim <blizzard777@gachon.ac.kr>
2024-07-03 15:14:02 +0900

    #2 QClass 새로 생성

---
+++ src/main/java/generated/University/Dormitory/domain/QPostUser.java
@@ -0,0 +1,40 @@
+package generated.University.Dormitory.domain;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import University.Dormitory.domain.PostUser;
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QPostUser is a Querydsl query type for PostUser
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QPostUser extends EntityPathBase<PostUser> {
+
+    private static final long serialVersionUID = 1128378604L;
+
+    public static final QPostUser postUser = new QPostUser("postUser");
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final DatePath<java.time.LocalDate> postWorkDate = createDate("postWorkDate", java.time.LocalDate.class);
+
+    public QPostUser(String variable) {
+        super(PostUser.class, forVariable(variable));
+    }
+
+    public QPostUser(Path<? extends PostUser> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QPostUser(PathMetadata metadata) {
+        super(PostUser.class, metadata);
+    }
+
+}
+
---
+++ src/main/java/generated/University/Dormitory/domain/QUser.java
@@ -0,0 +1,52 @@
+package generated.University.Dormitory.domain;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import University.Dormitory.domain.User;
+import University.Dormitory.domain.WorkDate;
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QUser is a Querydsl query type for User
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QUser extends EntityPathBase<User> {
+
+    private static final long serialVersionUID = -1868149588L;
+
+    public static final QUser user = new QUser("user");
+
+    public final EnumPath<University.Dormitory.domain.Enum.Authority> authority = createEnum("authority", University.Dormitory.domain.Enum.Authority.class);
+
+    public final EnumPath<University.Dormitory.domain.Enum.Dormitory> dormitory = createEnum("dormitory", University.Dormitory.domain.Enum.Dormitory.class);
+
+    public final DatePath<java.time.LocalDate> joinDate = createDate("joinDate", java.time.LocalDate.class);
+
+    public final StringPath name = createString("name");
+
+    public final StringPath password = createString("password");
+
+    public final NumberPath<Integer> userId = createNumber("userId", Integer.class);
+
+    public final ListPath<WorkDate, QWorkDate> workDates = this.<WorkDate, QWorkDate>createList("workDates", WorkDate.class, QWorkDate.class, PathInits.DIRECT2);
+
+    public QUser(String variable) {
+        super(User.class, forVariable(variable));
+    }
+
+    public QUser(Path<? extends User> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QUser(PathMetadata metadata) {
+        super(User.class, metadata);
+    }
+
+}
+
