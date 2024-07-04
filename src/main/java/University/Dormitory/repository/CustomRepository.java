c0bbec4f3e02a56e247075855d738d4873672c06
kwon-halim <blizzard777@gachon.ac.kr>
2024-07-04 23:28:39 +0900

    #5 근무 관련 DB 로직들 추가

--- src/main/java/University/Dormitory/repository/CustomRepository.java
+++ src/main/java/University/Dormitory/repository/CustomRepository.java
@@ -1,11 +1,19 @@
 package University.Dormitory.repository;
 
 import University.Dormitory.domain.Enum.Authority;
+import University.Dormitory.domain.WorkDate;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static University.Dormitory.domain.QWorkDate.workDate;
 import static generated.University.Dormitory.domain.QUser.user;
 
 @Slf4j
@@ -35,4 +43,29 @@ public class CustomRepository {
                 .fetchOne();
         return authority;
     }
+
+    public List<Integer> findUserIdsByDate(LocalDate date) {
+        List<Integer> userIdList = new ArrayList<>();
+
+        return query.select(workDate.user.userId)
+                .from(workDate)
+                .where(Expressions.dateTemplate(LocalDate.class, "DATE_FORMAT({0}, {1})", workDate.actualLeaveTime, "%Y-%m-%d").eq(date))
+                .fetch();
+    }
+
+    public long changeWorkDate(int userId1, int userId2, LocalDateTime userId1Date) {
+        return query.update(workDate)
+                .set(workDate.user.userId, userId2)
+                .where(workDate.user.userId.eq(userId1).and(workDate.scheduledStartTime.eq(userId1Date)))
+                .execute();
+    }
+
+    public long writeReason(int userID, String reason, LocalDateTime date) {
+        return  query.update(workDate)
+                .set(workDate.Reason, reason)
+                .where(workDate.user.userId.eq(userID).and(workDate.scheduledStartTime.eq(date)))
+                .execute();
+    }
+
+
 }
