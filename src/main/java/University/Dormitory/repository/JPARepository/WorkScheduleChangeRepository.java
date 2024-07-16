d0c0552df1174a2f08837618cc4c138958c43c37
kwon-halim <blizzard777@gachon.ac.kr>
2024-07-16 11:27:02 +0900

    #3 근무변경히스토리 JPARepository

---
+++ src/main/java/University/Dormitory/repository/JPARepository/WorkScheduleChangeRepository.java
@@ -0,0 +1,7 @@
+package University.Dormitory.repository.JPARepository;
+
+import University.Dormitory.domain.WorkScheduleChange;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WorkScheduleChangeRepository extends JpaRepository<WorkScheduleChange, Long> {
+}
