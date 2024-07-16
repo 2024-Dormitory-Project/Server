9fd6ae3f373ac9beb6632a802fcb029a454e3e3d
kwon-halim <blizzard777@gachon.ac.kr>
2024-07-16 11:08:04 +0900

    #3 간단하게 ApiResponse가 아닌 걸로 예외 처리 예정

--- src/main/java/University/Dormitory/exception/GlobalExceptionHandler.java
+++ src/main/java/University/Dormitory/exception/GlobalExceptionHandler.java
@@ -1,23 +1,31 @@
 package University.Dormitory.exception;
 
-import University.Dormitory.apiPayLoad.ApiResponse;
-import University.Dormitory.web.dto.SignInDTO.SignInResponseDTO;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import static University.Dormitory.apiPayLoad.code.ErrorCode.WRONG_PASSWROD;
 
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler(SignInFailException.class)
-    public ApiResponse<SignInResponseDTO.SignInFail> handleSignInFailException(SignInFailException e) {
-        log.error("로그인 실패: {}", e.getMessage());
-        SignInResponseDTO.SignInFail result = SignInResponseDTO.SignInFail.builder()
-                .message(e.getMessage())
-                .build();
-        return ApiResponse.onFailure(WRONG_PASSWROD.getCode(),e.getMessage(), result);
-    }
+//    @ExceptionHandler(PasswordNotMatchException.class)
+//    public ApiResponse<SignInResponseDTO.SignInFail> handlePasswordNotMatchException(PasswordNotMatchException e) {
+//        log.error("로그인 실패: {}", e.getMessage());
+//        SignInResponseDTO.SignInFail result = SignInResponseDTO.SignInFail.builder()
+//                .message(e.getMessage())
+//                .build();
+//        return ApiResponse.onFailure(WRONG_PASSWROD.getCode(),e.getMessage(), result);
+//    }
+//    @ExceptionHandler(PKDuplicateException.class)
+//    public ApiResponse<Void> handlePKDuplicate(PKDuplicateException e) {
+//        log.error("PK중복 오류: {}", e.getMessage());
+//        return ApiResponse.onFailure(PK_DUPLICATE.getCode(), e.getMessage());
+//    }
+//    @ExceptionHandler(UserNotFoundException.class)
+//    public ApiResponse<Void> handleUserNotFound(UserNotFoundException e) {
+//        return ApiResponse.onFailure(USER_NOT_FOUND.getCode(),e.getMessage());
+//    }
 
+//    @ExceptionHandler
+//    public ApiResponse<Void> handleTooLong(TooLongException e) {
+//        return ApiResponse.onFailure()
+//    }
 }
