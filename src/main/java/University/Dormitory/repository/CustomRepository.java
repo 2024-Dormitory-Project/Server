571f880f8d370d32907d997e51ac587964ce016f
kwon-halim <blizzard777@gachon.ac.kr>
2024-07-20 16:04:38 +0900

    #3 이번달 나의 일한 날짜 가져오기 쿼리 추가

--- src/main/java/University/Dormitory/repository/CustomRepository.java
+++ src/main/java/University/Dormitory/repository/CustomRepository.java
@@ -3,6 +3,7 @@ package University.Dormitory.repository;
 import University.Dormitory.domain.Enum.Authority;
 import University.Dormitory.domain.Enum.Dormitory;
 import University.Dormitory.domain.WorkDate;
+import University.Dormitory.domain.WorkScheduleChange;
 import com.querydsl.core.Tuple;
 import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
@@ -21,6 +22,7 @@ import java.util.concurrent.ConcurrentHashMap;
 
 import static University.Dormitory.domain.QUser.user;
 import static University.Dormitory.domain.QWorkDate.workDate;
+import static University.Dormitory.domain.QWorkScheduleChange.workScheduleChange;
 
 @Slf4j
 @Repository
@@ -33,14 +35,14 @@ public class CustomRepository {
         this.query = new JPAQueryFactory(em);
     }
 
-    public String findPasswordByUserId(int userId) {
+    public String findPasswordByUserId(long userId) {
         return query.select(user.password).
                 from(user)
                 .where(user.userId.eq(userId))
                 .fetchOne();
     }
 
-    public Authority findAuthoritiesByUserId(int userId) {
+    public Authority findAuthoritiesByUserId(long userId) {
         log.info("[findAuthoritiesByUserId] : 권한 정보 찾기 실행. 토큰발행용");
         Authority authority = query
                 .select(user.authority)
@@ -51,8 +53,8 @@ public class CustomRepository {
     }
 
     //    안쓰는 것. 아마 특정일에 일하는 모든 근무자들 날짜에 의해서만 가져오는 쿼리문인듯
-    public List<Integer> findUserIdsByDate(LocalDate date) {
-        List<Integer> userIdList = new ArrayList<>();
+    public List<Long> findUserIdsByDate(LocalDate date) {
+        List<Long> userIdList = new ArrayList<>();
 
         return query.select(workDate.user.userId)
                 .from(workDate)
@@ -60,14 +62,14 @@ public class CustomRepository {
                 .fetch();
     }
 
-    public long changeWorkDate(int userId1, int userId2, LocalDateTime userId1Date) {
+    public long changeWorkDate(long userId1, long userId2, LocalDateTime userId1Date) {
         return query.update(workDate)
                 .set(workDate.user.userId, userId2)
                 .where(workDate.user.userId.eq(userId1).and(workDate.scheduledStartTime.eq(userId1Date)))
                 .execute();
     }
 
-    public long writeReason(int userID, String reason, LocalDateTime date) {
+    public long writeReason(long userID, String reason, LocalDateTime date) {
         return query.update(workDate)
                 .set(workDate.Reason, reason)
                 .where(workDate.user.userId.eq(userID).and(workDate.scheduledStartTime.eq(date)))
@@ -97,7 +99,7 @@ public class CustomRepository {
         return dormitoryWorkers;
     }
 
-    public int countWorkDays(int userId, int year, int month) {
+    public int countWorkDays(long userId, int year, int month) {
         String monthString = String.format("%04d-%02d", year, month);
 
         List<WorkDate> list = query.selectFrom(workDate)
@@ -107,12 +109,8 @@ public class CustomRepository {
         return list.size();
     }
 
-    public List<Tuple> findWorksByUserIdAndDate(int userId, int year, int month) {
+    public List<Tuple> findWorksByUserIdAndDate(long userId, int year, int month) {
         String monthString = String.format("%04d-%02d", year, month);
-
-        //        for (Tuple tuple : fetch) {
-//            LocalDateTime localDateTime = tuple.get(workDate.scheduledStartTime);
-//        }
         return query.select(workDate.scheduledStartTime, workDate.scheduledLeaveTime)
                 .from(workDate)
                 .where(workDate.user.userId.eq(userId)
@@ -120,7 +118,7 @@ public class CustomRepository {
                 .fetch();
     }
 
-    public int countWorkHours(int userId, int year, int month) {
+    public int countWorkHours(long userId, int year, int month) {
         String monthString = String.format("%04d-%02d", year, month);
 
         List<WorkDate> list = query.selectFrom(workDate)
@@ -137,24 +135,43 @@ public class CustomRepository {
         return (int) workedHour;
     }
 
-    public List<Tuple> findScheduleTimeByUsreId(int userId) {
+    public List<Tuple> findScheduleTimeByUsreId(long userId) {
         return query.select(workDate.scheduledStartTime, workDate.scheduledLeaveTime)
                 .from(workDate)
                 .where(workDate.user.userId.eq(userId))
                 .fetch();
     }
 
-//    public String findPostUserNameByDate(LocalDate date) {
-//        return query.selectFrom(postUser.name)
-//                .where(postUser.postWorkDate.eq(date))
-//    }
 
-
-    public Dormitory findDormitoryByUserId(int userId) {
+    public Dormitory findDormitoryByUserId(long userId) {
         return query.select(user.dormitory)
                 .from(user)
                 .where(user.userId.eq((userId)))
                 .fetchOne();
+    }
+
+    public List<LocalDate> getExistingWorkDates(long userId, int year, int month) {
+        List<LocalDateTime> fetch = query
+                .select(workDate.scheduledStartTime)
+                .from(workDate)
+                .where(workDate.user.userId.eq(userId)
+                        .and(workDate.scheduledStartTime.year().eq(year))
+                        .and(workDate.scheduledStartTime.month().eq(month)))
+                .distinct()
+                .fetch();
+        List<LocalDate> exist = new ArrayList<>();
+        for (LocalDateTime localDateTime : fetch) {
+            LocalDate localDate = localDateTime.toLocalDate();
+            exist.add(localDate);
+        }
+        return exist;
+    }
+
+    public List<WorkScheduleChange> getWorkChangeHistoryByAcceptorUserId(long userId, int year, int month) {
+        return query
+                .selectFrom(workScheduleChange)
+                .where(workScheduleChange.acceptor.userId.eq(userId))
+                .fetch();
     }
 
     @Getter
