6de66f1a2cfa09e5e5cfade7bd325607512ecf42
kwon-halim <blizzard777@gachon.ac.kr>
2024-07-20 16:05:49 +0900

    #4 Refresh토큰 로직 추가

--- src/main/java/University/Dormitory/service/UserService/UserCommandServiceImpl.java
+++ src/main/java/University/Dormitory/service/UserService/UserCommandServiceImpl.java
@@ -10,8 +10,6 @@ import University.Dormitory.exception.Handler.UserNotFoundException;
 import University.Dormitory.repository.CustomRepository;
 import University.Dormitory.repository.JPARepository.UserRepository;
 import University.Dormitory.security.JwtTokenProvider;
-import University.Dormitory.web.dto.SignInDTO.SignInRequestDTO;
-import University.Dormitory.web.dto.SignInDTO.SignInResponseDTO;
 import University.Dormitory.web.dto.SignUpDTO.SignUpRequestDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -31,72 +29,77 @@ public class UserCommandServiceImpl implements UserCommandService {
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
     private final CustomRepository customRepository;
     private final JwtTokenProvider jwtTokenProvider;
-    
-//    인터페이스에는 없지만, 쓰이는 함수
-    private User findUserById(int userId) {
+
+    //    인터페이스에는 없지만, 쓰이는 함수
+    private User findUserById(long userId) {
         return userRepository.findById((long) userId)
                 .orElseThrow();
     }
 
 
-//    해당 함수 사용 시, authority는 converter 이용해서 넘겨줄 것.
+    //    해당 함수 사용 시, authority는 converter 이용해서 넘겨줄 것.
     @Override
-    public void updateAuthorityByUserId(int userId, Authority authority) {
+    public void updateAuthorityByUserId(long userId, Authority authority) {
         User user = findUserById(userId);
         user.setAuthority(authority);
         userRepository.save(user);
     }
 
     @Override
-    public void findAuthorityByUserId(int userId) {
+    public void findAuthorityByUserId(long userId) {
         User user = findUserById(userId);
     }
 
     @Override
-    public boolean checkUserIdDuplicate(Long userId) {
+    public boolean checkUserIdDuplicate(long userId) {
         return userRepository.existsById(userId);
     }
 
     @Override
     public User SignUp(SignUpRequestDTO.SignUpDto user) {
-        if(checkUserIdDuplicate((long) user.getUserId())) {
+        if (checkUserIdDuplicate((long) user.getUserId())) {
             log.info("[학번 중복 발생]. UserAlreadyExistException 예외 처리합니다.");
             throw new UserAlreadyExistException("유저가 이미 존재합니다");
         }
         User newUser = UserConverter.toUser(user); //유저 생성
-        newUser.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+//        newUser.setPassword(bCryptPasswordEncoder.encode(user.getPassword())); -> 비밀번호 인코딩 X
+        newUser.setPassword(user.getName());
         User savedUser = userRepository.save(newUser); // User 엔티티 저장
         return savedUser;
     }
 
     @Override
-    public SignInResponseDTO.SignInDto SignIn(SignInRequestDTO.SignInDto user) {
-        int userId = user.getUserId();
+    public String SignIn(Long userId, String pw) {
         String userName = "";
-        Optional<User> byId = userRepository.findById((long) userId);
-        if (byId.isPresent()) {
-            userName = byId.get().getName();
+        Optional<User> user = userRepository.findById(userId);
+        if (user.isPresent()) {
+            userName = user.get().getName();
         } else {
             log.info("로그인에 실패했습니다. ");
             throw new UserNotFoundException("해당 학번 조교가 존재하지 않습니다..");
         }
 
         log.info("userId:{}", userId);
-        log.info("전달받은 비밀번호:{}, 데이터베이스 비밀번호:{}", user.getPassword(), customRepository.findPasswordByUserId(userId));
-        if (!bCryptPasswordEncoder.matches(user.getPassword(), customRepository.findPasswordByUserId(userId))) {
+        log.info("전달받은 비밀번호:{}, 데이터베이스 비밀번호:{}", user.get().getPassword(), customRepository.findPasswordByUserId(userId));
+        if (!user.get().getPassword().equals(customRepository.findPasswordByUserId(userId))) {
             log.info("[패스워드 틀림] PasswordNotMatchException 예외처리합니다.");
             throw new PasswordNotMatchException("패스워드가 일치하지 않습니다.");
         }
         log.info("[패스워드 일치. 토큰 발급]");
         Authority authoritiesByUserId = customRepository.findAuthoritiesByUserId(userId);
         Dormitory dormitory = customRepository.findDormitoryByUserId(userId);
-        return SignInResponseDTO.SignInDto.builder()
-                .token(jwtTokenProvider.createAccessToken(String.valueOf(userId), authoritiesByUserId,userName,dormitory))
-                .build();
+
+        // Access Token 생성
+        String accessToken = jwtTokenProvider.createAccessToken(String.valueOf(userId), authoritiesByUserId, userName, dormitory);
+
+        // Refresh Token 생성
+//        String refreshToken = jwtTokenProvider.createRefreshToken(String.valueOf(userId));
+
+        return accessToken;
     }
 
-    public String SignOut(int userId) {
-        userRepository.deleteById((long) userId);
+    public String SignOut(long userId) {
+        userRepository.deleteById(userId);
         return "조교가 나갔습니다..";
     }
 }
