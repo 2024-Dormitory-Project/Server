c9af409351884a296a48c0dc9cd0b593e6ec8ba5
kwon-halim <blizzard777@gachon.ac.kr>
2024-07-20 16:03:29 +0900

    #2 return dto 추가

--- src/main/java/University/Dormitory/web/dto/WorkDateDTO/WorkDateResponseDTO.java
+++ src/main/java/University/Dormitory/web/dto/WorkDateDTO/WorkDateResponseDTO.java
@@ -1,10 +1,30 @@
 package University.Dormitory.web.dto.WorkDateDTO;
 
+import University.Dormitory.domain.WorkDate;
+import lombok.Builder;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class WorkDateResponseDTO {
 
     public static class payRollDto{
         int thisMonth;
         int workDays;
         int workHours;
+    }
+
+    @Builder
+    public static class calendarDto {
+        List<LocalDate> workdates;
+    }
+    @Builder
+    public static class changeHistory {
+        String status;
+        String applicant;
+        String acceptor;
+        LocalDateTime originDate;
+        LocalDateTime changeDate;
     }
 }
