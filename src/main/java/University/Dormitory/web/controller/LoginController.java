52426c77d74fae80a7a0a701213e9d2a7f245a30
kwon-halim <blizzard777@gachon.ac.kr>
2024-07-20 16:05:02 +0900

    #3 컨트롤러 URL 수정 및 return DTO 수정

--- src/main/java/University/Dormitory/web/controller/LoginController.java
+++ src/main/java/University/Dormitory/web/controller/LoginController.java
@@ -1,19 +1,18 @@
 package University.Dormitory.web.controller;
 
-import University.Dormitory.apiPayLoad.ApiResponse;
 import University.Dormitory.domain.User;
 import University.Dormitory.service.UserService.UserCommandService;
-import University.Dormitory.web.dto.SignInDTO.SignInRequestDTO;
 import University.Dormitory.web.dto.SignInDTO.SignInResponseDTO;
 import University.Dormitory.web.dto.SignOutDTO.SignOutRequestDTO;
+import University.Dormitory.web.dto.SignOutDTO.SignOutResponseDTO;
 import University.Dormitory.web.dto.SignUpDTO.SignUpRequestDTO;
 import University.Dormitory.web.dto.SignUpDTO.SignUpResponseDTO;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
-
-import java.time.LocalDate;
 
 @RestController
 @RequiredArgsConstructor
@@ -24,16 +23,23 @@ public class LoginController {
     private final UserCommandService userCommandService;
 
     @PostMapping("/signin")
-    public SignInResponseDTO.SignInDto SignIn(@RequestBody @Valid SignInRequestDTO.SignInDto request) {
+    public SignInResponseDTO.SignInSuccess SignIn(HttpServletRequest request,
+                                                  @JsonProperty("ID") Long userId, @JsonProperty("PW") String pw) {
         log.info("[PostMapping] [SignIn 실행]");
-        log.info("로그인 시도 정보: id : {}, pw : {}", request.getUserId(), request.getPassword());
-        SignInResponseDTO.SignInDto signInDto = userCommandService.SignIn(request);
-        log.info("로그인 성공. SignInResponseDTO.SignInDTO 객체 반환");
-
-//       헤더에다가는 accessToken, Body에다가는 refreshToken 발급
-//        .setHeader()사용
-
+        log.info("로그인 시도 정보: id : {}, pw : {}", userId, pw);
+        String accessToken = userCommandService.SignIn(userId, pw);
+        log.info("로그인 성공. AccessToken 반환");
+        return SignInResponseDTO.SignInSuccess.builder()
+                .token("Bearer " + accessToken)
+                .message("로그인 성공")
+                .isSuccess(true)
+                .build();
     }
+
+//    @GetMapping("/refresh")
+//    public SignInResponseDTO.SignInSuccess RefreshToken() {
+//
+//    }
 
     @GetMapping("/")
     public String home() {
@@ -46,24 +52,26 @@ public class LoginController {
         return "login";
     }
 
-    @PostMapping("/signup")
+    @PostMapping("/perfect/signup")
     public SignUpResponseDTO.SignUpDto SignUp(@RequestBody @Valid SignUpRequestDTO.SignUpDto request) {
         log.info("[PostMapping] [SignUp 실행]");
-        log.info("회원가입 시도, 권한정보:{}, 기숙사:{}, 이름:{}, 학번:{}, 비밀번호=이름:{}", request.getAuthority(), request.getDormitory(), request.getName(),
-                request.getUserId(), request.getPassword());
+        log.info("회원가입 시도, 권한정보:{}, 기숙사:{}, 이름:{}, 학번:{}", request.getAuthority(), request.getDormitory(), request.getName(),
+                request.getUserId());
         User user = userCommandService.SignUp(request);
-        SignUpResponseDTO.SignUpDto result = SignUpResponseDTO.SignUpDto.builder()
-                .name(user.getName())
-                .userId(user.getUserId())
-                .date(LocalDate.now())
-                .build();
-        return ApiResponse.onSuccess(result);
+        return SignUpResponseDTO.SignUpDto.builder()
+                .isSuccess(true)
+                .message("회원 정보 저장 성공").
+                build();
     }
 
-    @PostMapping("/signout")
-    public String SignOut(@RequestBody @Valid SignOutRequestDTO.SignOutDTO request) {
+    @PostMapping("/perfect/signout")
+    public SignOutResponseDTO.SignOutDTO SignOut(@RequestBody @Valid SignOutRequestDTO.SignOutDTO request) {
         log.info("[회원탈퇴 시도], 탈퇴하려는 학번:{}, 탈퇴하려는 이름:{}", request.getUserId(), request.getName());
         userCommandService.SignOut(request.getUserId());
-        return ApiResponse.onSuccess("오늘도 하나의 조교가 나갔습니다...");
+        return SignOutResponseDTO.SignOutDTO
+                .builder()
+                .isSuccess(true)
+                .message("조교가 사라졌습니다")
+                .build();
     }
 }
