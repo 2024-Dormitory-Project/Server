99e62920b220d390778b0141a24cfb72d19e643e
kwon-halim <blizzard777@gachon.ac.kr>
2024-07-20 16:03:48 +0900

    #2 이번달 나의 일한 날 DTO 추가

--- src/main/java/University/Dormitory/service/DateService/DateCommandService.java
+++ src/main/java/University/Dormitory/service/DateService/DateCommandService.java
@@ -2,10 +2,16 @@ package University.Dormitory.service.DateService;
 
 import University.Dormitory.web.dto.WorkDateDTO.WorkDateRequestDTO;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface DateCommandService {
 //    지금까지 내가 일한 시간 쿼리
-    int workHour(int year, int workMonth, int userId);
+    int workHour(int year, int workMonth, long userId);
 
 //    지금까지 내가 일한 날짜 쿼리
-    int workDays(int year, int workMonth, int userId);
+    int workDays(int year, int workMonth, long userId);
+
+//    이번 달 나의 근무일 보기
+    List<LocalDate> myworkdays (int year, int month, long userId);
 }
--- src/main/java/University/Dormitory/service/DateService/DateCommandServiceImpl.java
+++ src/main/java/University/Dormitory/service/DateService/DateCommandServiceImpl.java
@@ -7,16 +7,18 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @RequiredArgsConstructor
 @Service
 @Transactional
 @Slf4j
 public class DateCommandServiceImpl implements DateCommandService {
-    private final WorkDateRepository workDateRepository;
     private final CustomRepository customRepository;
 
     @Override
-    public int workHour(int year, int workMonth, int userId) {
+    public int workHour(int year, int workMonth, long userId) {
         try {
             return customRepository.countWorkHours(userId, year, workMonth);
         } catch (Exception e) {
@@ -25,11 +27,16 @@ public class DateCommandServiceImpl implements DateCommandService {
     }
 
     @Override
-    public int workDays(int year, int workMonth, int userId) {
+    public int workDays(int year, int workMonth, long userId) {
         try {
             return customRepository.countWorkDays(userId, year, workMonth);
         } catch (Exception e) {
             throw new RuntimeException("근무일 계산 중 오류가 발생하였습니다. " + e.getMessage());
         }
+    }
+
+    @Override
+    public List<LocalDate> myworkdays(int year, int month, long userId) {
+        return null;
     }
 }
