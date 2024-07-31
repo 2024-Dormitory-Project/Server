1119242c2cd2b7f9c8a211242181fbb203b70da8
kwon-halim <blizzard777@gachon.ac.kr>
2024-07-31 11:17:31 +0900

    #3 DTO 수정

---
+++ src/main/java/University/Dormitory/web/dto/ScheduleDTO/ScheduleResponseDTO.java
@@ -0,0 +1,24 @@
+package University.Dormitory.web.dto.ScheduleDTO;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class ScheduleResponseDTO {
+    @Builder
+    @Getter
+    public static class actualWorkTime {
+        LocalDate date;
+        LocalTime scheduleTime;
+        LocalTime actualTime;
+        String name;
+
+    }
+}
