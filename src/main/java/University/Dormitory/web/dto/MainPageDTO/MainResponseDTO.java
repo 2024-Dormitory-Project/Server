9245b3981b51ff012a73fbd55dd47dbc61979a99
kwon-halim <blizzard777@gachon.ac.kr>
2024-08-01 02:27:32 +0900

    Servelet단계에서 Return용 객체 생성하려 했으나 안됨. 다시 바꿔야함

--- src/main/java/University/Dormitory/web/dto/MainPageDTO/MainResponseDTO.java
+++ src/main/java/University/Dormitory/web/dto/MainPageDTO/MainResponseDTO.java
@@ -2,6 +2,7 @@ package University.Dormitory.web.dto.MainPageDTO;
 
 import University.Dormitory.domain.WorkScheduleChange;
 import University.Dormitory.repository.CustomRepository;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.querydsl.core.Tuple;
 import lombok.*;
 import lombok.extern.slf4j.Slf4j;
@@ -11,6 +12,7 @@ import java.util.List;
 
 public class MainResponseDTO {
     @Builder
+    @Getter
     public static class TimeDto {
         int day;
         int time;
@@ -21,6 +23,14 @@ public class MainResponseDTO {
     @NoArgsConstructor
     @AllArgsConstructor
     public static class Work{
+        Boolean isSuccess;
+        String message;
+    }
+
+    @JsonDeserialize(builder = ServeletResponse.class)
+    @Setter
+    @Getter
+    public static class ServeletResponse {
         Boolean isSuccess;
         String message;
     }
