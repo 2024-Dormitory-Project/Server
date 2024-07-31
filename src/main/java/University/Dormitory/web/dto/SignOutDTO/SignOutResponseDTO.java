dec0f54bb5e29bad5049901b38099b3807ca431c
kwon-halim <blizzard777@gachon.ac.kr>
2024-08-01 02:25:51 +0900

    #3 @Getter 없을 시 Json변환에 오류 있었음 Could not find acceptable representation~~오류

--- src/main/java/University/Dormitory/web/dto/SignOutDTO/SignOutResponseDTO.java
+++ src/main/java/University/Dormitory/web/dto/SignOutDTO/SignOutResponseDTO.java
@@ -1,9 +1,11 @@
 package University.Dormitory.web.dto.SignOutDTO;
 
 import lombok.Builder;
+import lombok.Getter;
 
 public class SignOutResponseDTO {
     @Builder
+    @Getter
     public static class SignOutDTO {
         boolean isSuccess;
         String message;
