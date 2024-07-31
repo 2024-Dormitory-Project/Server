131e21c7551122f1247448a3f6b1491456178d7c
kwon-halim <blizzard777@gachon.ac.kr>
2024-07-31 11:18:38 +0900

    쿼리문 변경

--- src/main/java/University/Dormitory/repository/CustomRepository.java
+++ src/main/java/University/Dormitory/repository/CustomRepository.java
@@ -2,23 +2,24 @@ package University.Dormitory.repository;
 
 import University.Dormitory.domain.Enum.Authority;
 import University.Dormitory.domain.Enum.Dormitory;
+import University.Dormitory.domain.User;
 import University.Dormitory.domain.WorkDate;
 import University.Dormitory.domain.WorkScheduleChange;
 import com.querydsl.core.Tuple;
-import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.Optional;
 
 import static University.Dormitory.domain.QUser.user;
 import static University.Dormitory.domain.QWorkDate.workDate;
@@ -35,6 +36,11 @@ public class CustomRepository {
         this.query = new JPAQueryFactory(em);
     }
 
+    public User findByUserId(long userId) {
+        return query.selectFrom(user)
+                .where(user.userId.eq(userId))
+                .fetchOne();
+    }
     public String findPasswordByUserId(long userId) {
         return query.select(user.password).
                 from(user)
@@ -44,22 +50,11 @@ public class CustomRepository {
 
     public Authority findAuthoritiesByUserId(long userId) {
         log.info("[findAuthoritiesByUserId] : 권한 정보 찾기 실행. 토큰발행용");
-        Authority authority = query
+        return query
                 .select(user.authority)
                 .from(user)
                 .where(user.userId.eq(userId))
                 .fetchOne();
-        return authority;
-    }
-
-    //    안쓰는 것. 아마 특정일에 일하는 모든 근무자들 날짜에 의해서만 가져오는 쿼리문인듯
-    public List<Long> findUserIdsByDate(LocalDate date) {
-        List<Long> userIdList = new ArrayList<>();
-
-        return query.select(workDate.user.userId)
-                .from(workDate)
-                .where(Expressions.dateTemplate(LocalDate.class, "DATE_FORMAT({0}, {1})", workDate.actualLeaveTime, "%Y-%m-%d").eq(date))
-                .fetch();
     }
 
     public long changeWorkDate(long userId1, long userId2, LocalDateTime userId1Date) {
@@ -76,13 +71,15 @@ public class CustomRepository {
                 .execute();
     }
 
-    public Map<String, WorkTime> findDormitoryWorkersNameByDateAndDormitory(LocalDate date, Dormitory dormitory) {
-        Map<String, WorkTime> dormitoryWorkers = new ConcurrentHashMap<>();
+    public MultiValueMap<String, WorkTime> findDormitoryWorkersNameByDateAndDormitory(LocalDate date, Dormitory dormitory) {
+        MultiValueMap<String, WorkTime> dormitoryWorkers = new LinkedMultiValueMap<>();
 
         List<Tuple> fetchWorkTimesAndNames = query
                 .select(workDate.user.name, workDate.scheduledStartTime, workDate.scheduledLeaveTime)
                 .from(workDate)
-                .where(Expressions.dateTemplate(LocalDate.class, "DATE_FORMAT({0}, {1})", workDate.scheduledStartTime, "%Y-%m-%d").eq(date)
+                .where(workDate.scheduledStartTime.year().eq(date.getYear())
+                        .and(workDate.scheduledStartTime.month().eq(date.getMonthValue()))
+                        .and(workDate.scheduledStartTime.dayOfMonth().eq(date.getDayOfMonth()))
                         .and(workDate.user.dormitory.eq(dormitory)))
                 .fetch();
 
@@ -93,37 +90,55 @@ public class CustomRepository {
 
             WorkTime workTime = new WorkTime(scheduledStartTime, scheduledLeaveTime);
             log.info("[이름] : {}, 시작시간:{}, 끝나는 시간:{}", name, scheduledStartTime, scheduledLeaveTime);
-            dormitoryWorkers.put(name, workTime);
+
+            dormitoryWorkers.add(name, workTime);
         }
 
         return dormitoryWorkers;
     }
 
-    public int countWorkDays(long userId, int year, int month) {
-        String monthString = String.format("%04d-%02d", year, month);
 
+    //    public int countWorkDays(long userId, int year, int month) {
+//        String monthString = String.format("%04d-%02d", year, month);
+//
+//        List<WorkDate> list = query.selectFrom(workDate)
+//                .where(workDate.user.userId.eq(userId)
+//                        .and(Expressions.dateTemplate(String.class, "DATE_FORMAT({0}, {1})", workDate.scheduledStartTime, "%Y-%m").eq(monthString)))
+//                .fetch();
+//        return list.size();
+//    }아래와 동일
+    public int countWorkDays(long userId, int year, int month) {
         List<WorkDate> list = query.selectFrom(workDate)
                 .where(workDate.user.userId.eq(userId)
-                        .and(Expressions.dateTemplate(String.class, "DATE_FORMAT({0}, {1})", workDate.scheduledStartTime, "%Y-%m").eq(monthString)))
+                        .and(workDate.scheduledStartTime.year().eq(year))
+                        .and(workDate.scheduledStartTime.month().eq(month)))
                 .fetch();
         return list.size();
     }
 
-    public List<Tuple> findWorksByUserIdAndDate(long userId, int year, int month) {
-        String monthString = String.format("%04d-%02d", year, month);
-        return query.select(workDate.scheduledStartTime, workDate.scheduledLeaveTime)
-                .from(workDate)
-                .where(workDate.user.userId.eq(userId)
-                        .and(Expressions.dateTemplate(String.class, "DATE_FORMAT({0}, {1})", workDate.scheduledStartTime, "%Y-%m").eq(monthString)))
-                .fetch();
-    }
-
+    //    public int countWorkHours(long userId, int year, int month) {
+//        String monthString = String.format("%04d-%02d", year, month);
+//
+//        List<WorkDate> list = query.selectFrom(workDate)
+//                .where(workDate.user.userId.eq(userId)
+//                        .and(Expressions.dateTemplate(String.class, "DATE_FORMAT({0}, '%Y-%m')", workDate.scheduledStartTime).eq(monthString)))
+//                .fetch();
+//
+//        long workedHour = 0;
+//        for (WorkDate date : list) {
+//            Duration duration = Duration.between(date.getScheduledStartTime(), date.getScheduledLeaveTime());
+//            workedHour += duration.toHours();
+//        }
+//
+//        return (int) workedHour;
+//    }아래와 동일
     public int countWorkHours(long userId, int year, int month) {
         String monthString = String.format("%04d-%02d", year, month);
 
         List<WorkDate> list = query.selectFrom(workDate)
                 .where(workDate.user.userId.eq(userId)
-                        .and(Expressions.dateTemplate(String.class, "DATE_FORMAT({0}, '%Y-%m')", workDate.scheduledStartTime).eq(monthString)))
+                        .and(workDate.scheduledStartTime.year().eq(year))
+                        .and(workDate.scheduledStartTime.month().eq(month)))
                 .fetch();
 
         long workedHour = 0;
@@ -135,13 +150,21 @@ public class CustomRepository {
         return (int) workedHour;
     }
 
-    public List<Tuple> findScheduleTimeByUsreId(long userId) {
-        return query.select(workDate.scheduledStartTime, workDate.scheduledLeaveTime)
+    //    public List<Tuple> findScheduleTimeAndActualTimeByUserId(long userId, LocalDate date) {
+//        return query.select(workDate.scheduledStartTime, workDate.actualStartTime, workDate.user.name)
+//                .from(workDate)
+//                .where(workDate.user.userId.eq(userId)
+//                        .and(Expressions.dateTemplate(String.class, "DATE_FORMAT({0}, '%Y-%m')", workDate.scheduledStartTime).eq(String.valueOf(date))))
+//                .fetch();
+//    }아래와동일
+    public List<Tuple> findScheduleTimeAndActualTimeByUserId(long userId, LocalDate date) {
+        return query.select(workDate.scheduledStartTime, workDate.actualStartTime, workDate.user.name)
                 .from(workDate)
-                .where(workDate.user.userId.eq(userId))
+                .where(workDate.user.userId.eq(userId)
+                        .and(workDate.scheduledStartTime.year().eq(date.getYear()))
+                        .and(workDate.scheduledStartTime.month().eq(date.getMonthValue())))
                 .fetch();
     }
-
 
     public Dormitory findDormitoryByUserId(long userId) {
         return query.select(user.dormitory)
@@ -167,11 +190,89 @@ public class CustomRepository {
         return exist;
     }
 
-    public List<WorkScheduleChange> getWorkChangeHistoryByAcceptorUserId(long userId, int year, int month) {
+
+    /**
+     * 현재 사용자가 수락한 변경내역
+     *
+     * @param userId
+     * @param date   : 연,월로만 할 꺼니까 일은 상관없음.
+     * @return
+     */
+//    public List<WorkScheduleChange> getWorkChangeHistoryByAcceptorUserId(long userId, LocalDate date) {
+//        return query
+//                .selectFrom(workScheduleChange)
+//                .where(Expressions.dateTemplate(String.class, "DATE_FORMAT({0}, '%Y-%m')", workDate.scheduledStartTime).eq(String.valueOf(date))
+//                        .and(workScheduleChange.acceptor.userId.eq(userId)))
+//                .fetch();
+//    }
+    public List<WorkScheduleChange> getWorkChangeHistoryByAcceptorUserId(long userId, LocalDate date) {
         return query
                 .selectFrom(workScheduleChange)
-                .where(workScheduleChange.acceptor.userId.eq(userId))
+                .where(workScheduleChange.acceptor.userId.eq(userId)
+                        .and(workScheduleChange.beforeChangeDate.year().eq(date.getYear()))
+                        .and(workScheduleChange.beforeChangeDate.month().eq(date.getMonthValue())))
                 .fetch();
+    }
+
+    /**
+     * 현재 사용자가 신청한 변경내역
+     *
+     * @param userId
+     * @param date   : 연,월로만 할 꺼니까 일은 상관없음.
+     * @return
+     */
+//    public List<WorkScheduleChange> getWorkChangeHistoryByApplicantUserId(long userId, LocalDate date) {
+//        return query
+//                .selectFrom(workScheduleChange)
+//                .where(Expressions.dateTemplate(String.class, "DATE_FORMAT({0}, '%Y-%m')", workDate.scheduledStartTime).eq(String.valueOf(date))
+//                        .and(workScheduleChange.applicant.userId.eq(userId)))
+//                .fetch();
+//    }
+    public List<WorkScheduleChange> getWorkChangeHistoryByApplicantUserId(long userId, LocalDate date) {
+        return query
+                .selectFrom(workScheduleChange)
+                .where(workScheduleChange.applicant.userId.eq(userId)
+                        .and(workScheduleChange.beforeChangeDate.year().eq(date.getYear()))
+                        .and(workScheduleChange.beforeChangeDate.month().eq(date.getMonthValue())))
+                .fetch();
+    }
+
+    public Optional<Long> findUserIdByName(String name) {
+        return Optional.ofNullable(query.select(user.userId)
+                .from(user)
+                .where(user.name.eq(name))
+                .fetchOne());
+    }
+
+
+    public String deleteSchedule(LocalDate date, Dormitory dormitory) {
+        query.delete(workDate)
+                .where(
+                        workDate.scheduledStartTime.year().eq(date.getYear())
+                                .and(workDate.scheduledStartTime.month().eq(date.getMonthValue()))
+                                .and(workDate.user.dormitory.eq(dormitory))
+                )
+                .execute();
+        log.info("삭제 성공");
+        return "해당 일 스케줄 삭제 성공";
+    }
+
+
+    public void updateActualStartTime(Long userId, LocalDateTime scheduledTime, LocalDateTime actualTime) {
+        query.update(workDate)
+                .where(workDate.scheduledStartTime.eq(scheduledTime)
+                        .and(workDate.user.userId.eq(userId)))
+                .set(workDate.actualStartTime, actualTime)
+                .execute();
+    }
+
+    public boolean existsByUserIdAndscheduledStartTime(long userId, LocalDateTime scheduledStartTime) {
+        List<WorkDate> results = query.selectFrom(workDate)
+                .where(workDate.scheduledStartTime.eq(scheduledStartTime)
+                        .and(workDate.user.userId.eq(userId)))
+                .fetch();
+
+        return !results.isEmpty();
     }
 
     @Getter
