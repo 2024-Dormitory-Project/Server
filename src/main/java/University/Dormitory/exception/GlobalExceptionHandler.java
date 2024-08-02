a32a8456f5f0b25b12b135fd64880e113f612e92
kwon-halim <blizzard777@gachon.ac.kr>
2024-08-02 23:15:09 +0900

    #3 응답 통일화 및 URL 예외 추가

--- src/main/java/University/Dormitory/exception/GlobalExceptionHandler.java
+++ src/main/java/University/Dormitory/exception/GlobalExceptionHandler.java
@@ -17,91 +17,98 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(InvalidTokenException.class)
     public ResponseEntity<ErrorResponse> handleInvalidTokenException(InvalidTokenException ex) {
         ErrorResponse errorResponse = new ErrorResponse(ex.getError(), ex.getMessage());
-        log.error("InvalidTokenException: {}", ex.getMessage(), ex);
+        log.error("InvalidTokenException: {}", ex.getMessage());
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(PasswordNotMatchException.class)
     public ResponseEntity<ErrorResponse> handlePasswordNotMatchException(PasswordNotMatchException ex) {
         ErrorResponse errorResponse = new ErrorResponse(ex.getError(), ex.getMessage());
-        log.error("PasswordNotMatchException: {}", ex.getMessage(), ex);
+        log.error("PasswordNotMatchException: {}", ex.getMessage());
         return new ResponseEntity<>(errorResponse, HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler(PKDuplicateException.class)
     public ResponseEntity<ErrorResponse> handlePKDuplicateException(PKDuplicateException ex) {
         ErrorResponse errorResponse = new ErrorResponse(ex.getError(), ex.getMessage());
-        log.error("PKDuplicateException: {}", ex.getMessage(), ex);
+        log.error("PKDuplicateException: {}", ex.getMessage());
         return new ResponseEntity<>(errorResponse, HttpStatus.CONFLICT);
     }
 
     @ExceptionHandler(RefreshTokenInvalidException.class)
     public ResponseEntity<ErrorResponse> handleRefreshTokenInvalidException(RefreshTokenInvalidException ex) {
         ErrorResponse errorResponse = new ErrorResponse(ex.getError(), ex.getMessage());
-        log.error("RefreshTokenInvalidException: {}", ex.getMessage(), ex);
+        log.error("RefreshTokenInvalidException: {}", ex.getMessage());
         return new ResponseEntity<>(errorResponse, HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler(TooLongException.class)
     public ResponseEntity<ErrorResponse> handleTooLongException(TooLongException ex) {
         ErrorResponse errorResponse = new ErrorResponse(ex.getError(), ex.getMessage());
-        log.error("TooLongException: {}", ex.getMessage(), ex);
+        log.error("TooLongException: {}", ex.getMessage());
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(UndefinedAuthorityException.class)
     public ResponseEntity<ErrorResponse> handleUndefinedAuthorityException(UndefinedAuthorityException ex) {
         ErrorResponse errorResponse = new ErrorResponse(ex.getError(), ex.getMessage());
-        log.error("UndefinedAuthorityException: {}", ex.getMessage(), ex);
+        log.error("UndefinedAuthorityException: {}", ex.getMessage());
         return new ResponseEntity<>(errorResponse, HttpStatus.FORBIDDEN);
     }
 
     @ExceptionHandler(UndefinedDormitoryException.class)
     public ResponseEntity<ErrorResponse> handleUndefinedDormitoryException(UndefinedDormitoryException ex) {
         ErrorResponse errorResponse = new ErrorResponse(ex.getError(), ex.getMessage());
-        log.error("UndefinedDormitoryException: {}", ex.getMessage(), ex);
+        log.error("UndefinedDormitoryException: {}", ex.getMessage());
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(UserAlreadyExistException.class)
     public ResponseEntity<ErrorResponse> handleUserAlreadyExistException(UserAlreadyExistException ex) {
         ErrorResponse errorResponse = new ErrorResponse(ex.getError(), ex.getMessage());
-        log.error("UserAlreadyExistException: {}", ex.getMessage(), ex);
+        log.error("UserAlreadyExistException: {}", ex.getMessage());
         return new ResponseEntity<>(errorResponse, HttpStatus.CONFLICT);
     }
 
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleUserNotFoundException(UserNotFoundException ex) {
         ErrorResponse errorResponse = new ErrorResponse(ex.getError(), ex.getMessage());
-        log.error("UserNotFoundException: {}", ex.getMessage(), ex);
+        log.error("UserNotFoundException: {}", ex.getMessage());
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(BadDateRequestException.class)
     public ResponseEntity<ErrorResponse> handleBadDateRequestException(BadDateRequestException ex) {
         ErrorResponse errorResponse = new ErrorResponse(ex.getError(), ex.getMessage());
-        log.error("BadDateRequestException: {}", ex.getMessage(), ex);
+        log.error("BadDateRequestException: {}", ex.getMessage());
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(AccessDeniedException.class)
     public ResponseEntity<ErrorResponse> handleAccessDeniedException(AccessDeniedException ex) {
         ErrorResponse errorResponse = new ErrorResponse(ex.getReason(), ex.getMessage());
-        log.error("BadDateRequestException: {}", ex.getMessage(), ex);
+        log.error("BadDateRequestException: {}", ex.getMessage());
         return new ResponseEntity<>(errorResponse, HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<ErrorResponse> handleRuntimeException(RuntimeException ex) {
         ErrorResponse errorResponse = new ErrorResponse("UNKNOWN_ERROR", ex.getMessage());
-        log.error("RuntimeException: {}", ex.getMessage(), ex);
+        log.error("RuntimeException: {}", ex.getMessage());
         return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(UndefinedWorkDate.class)
     public ResponseEntity<ErrorResponse> handleUndefinedWorkDate(UndefinedWorkDate ex) {
         ErrorResponse errorResponse = new ErrorResponse(ex.getError(), ex.getMessage());
-        log.error("UndefinedWorkDate: {}", ex.getMessage(), ex);
+        log.error("UndefinedWorkDate: {}", ex.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(WrongPathRequestException.class)
+    public ResponseEntity<ErrorResponse> handleWrongPathRequestException(WrongPathRequestException ex) {
+        ErrorResponse errorResponse = new ErrorResponse(ex.getError(), ex.getMessage());
+        log.error("WrongPath: {}", ex.getMessage());
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 }
