b86677f07c94a613b6c9230c32b4fc6c15cea04c
kwon-halim <blizzard777@gachon.ac.kr>
2024-08-09 16:17:29 +0900

    #38 :memo: Swagger를 통한 API 문서화 추가

--- src/main/java/University/Dormitory/web/dto/WorkDTO/WorkRequestDTO.java
+++ src/main/java/University/Dormitory/web/dto/WorkDTO/WorkRequestDTO.java
@@ -1,9 +1,8 @@
 package University.Dormitory.web.dto.WorkDTO;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
-import java.time.LocalDateTime;
-import java.util.HashMap;
 import java.util.List;
 
 public class WorkRequestDTO {
@@ -34,7 +33,18 @@ public class WorkRequestDTO {
 
     @Getter
     public static class worker {
+        @Schema(description = "이름", example = "권하림")
         String name;
+        @Schema(description = "이름", example = "\"times\":[\"2023-04-18 10:30\", \"2023-04-18 10:30\", \"2023-04-18 10:30\"] OR " +
+                "[\"2023-04-18 10:30\", \"2023-04-18 10:30\"]")
         List<String> times;
+    }
+
+    @Getter
+    public static class reason {
+        @Schema(description = "시간", example = "2024-04-18 11:30")
+        String time;
+        @Schema(description = "사유", example = "늦잠")
+        String reason;
     }
 }
